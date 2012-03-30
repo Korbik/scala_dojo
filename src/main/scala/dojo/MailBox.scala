b6e3e9a4c038648ade1db8822695502ba2d649b4
Cédric CORBIERE <corbic@gmail.com>
2012-03-30 21:09:09 +0200

    Implement all the module to match the specs

--- src/main/scala/dojo/MailBox.scala
+++ src/main/scala/dojo/MailBox.scala
@@ -1,11 +1,17 @@
 package dojo
 
+
 class MailBox {
 
   def handleMsg(msg: PingPongMessage) = msg match{
-    case _ => None
+    case Ping => Pong
+    case Pong => Ping
   }
 
-  def receive(msg: Any) = None
+  def receive(msg: Any) = msg match {
+    case m :PingPongMessage => handleMsg(m)
+    case "stop" => "stopped"
+    case ("echo",str) => str
+  }
 
 }
